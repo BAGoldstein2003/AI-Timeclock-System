0ce769033b7be160a3c7dfb73fdb2a2ddfda1b6c
BAGoldstein2003 <bagoldstein2003@gmail.com>
2025-03-14 05:26:56 -0400

    added error handling for clock-in and clock-out functions

--- views/ClockOut.py
+++ views/ClockOut.py
@@ -6,15 +6,18 @@ st.write("Please Clock-Out using the Facial Detection Camera below:")
 
 clockInImg = st.camera_input(label = "")
 if clockInImg:
-    with st.spinner("Detecting face..."):
-        clockInImgRaw = clockInImg.getvalue()
-        result = compare(clockInImgRaw, get_photo(st.session_state.name))
-        match result:
-            case 1:
-                st.session_state["clocked_in"] = False
-                st.session_state["clock_time"] = None
-                st.success("You successfully clocked out!")
-            case 0:
-                st.error(f"Face did not match {st.session_state.name}'s face")
-            case -1:
-                st.error("Your picture does not contain a face. Please retake the picture.")
+    if (st.session_state.clocked_in == False):
+        with st.spinner("Detecting face..."):
+            clockInImgRaw = clockInImg.getvalue()
+            result = compare(clockInImgRaw, get_photo(st.session_state.name))
+            match result:
+                case 1:
+                    st.session_state["clocked_in"] = False
+                    st.session_state["clock_time"] = None
+                    st.success("You successfully clocked out!")
+                case 0:
+                    st.error(f"Face did not match {st.session_state.name}'s face")
+                case -1:
+                    st.error("Your picture does not contain a face. Please retake the picture.")
+    else:
+        st.error("You are not clocked in! Please clock in before trying to clock out")
